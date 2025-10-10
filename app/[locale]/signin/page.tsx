f4968eb20625ae8fffb98f4676ea1c8eb8e715dd
AgilitiesZ <pakkung1245@gmail.com>
2025-10-10 23:26:31 +0700

    fixed Signin Page

--- app/[locale]/signin/page.tsx
+++ app/[locale]/signin/page.tsx
@@ -1,8 +1,8 @@
 import SigninPage from "@/views/signin/pages/Signin";
 export default function Signin() {
     return (
-        <div>
-            <SigninPage />
-        </div>
+
+        <SigninPage />
+
     );
 }
--- views/signin/components/AuthRight.tsx
+++ views/signin/components/AuthRight.tsx
@@ -4,7 +4,7 @@ import AuthTermsNotice from "./AuthTermsNotice"
 
 export default function AuthRight() {
     return (
-        <div className="py-8 lg:py-0 w-full h-screen flex flex-col justify-center items-center">
+        <div className="border py-8 lg:py-0 w-full h-screen flex flex-col justify-center items-center">
             <div className="w-full flex flex-col justify-center items-center gap-y-10 flex-1">
 
                 {/* LOGIN HEADER */}
